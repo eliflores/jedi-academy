9c896dc942e1adc5b64fd5869f347d93dfc58201
Ely Flores <elifloresch@gmail.com>
2016-08-09 21:25:45 -0700

    Added other 1 test for method in ArrayOperations left to implement.

--- programming-exercises/javaing/src/test/java/com/programming/exercises/ArrayOperationsTest.java
+++ programming-exercises/javaing/src/test/java/com/programming/exercises/ArrayOperationsTest.java
@@ -6,9 +6,10 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class ArrayOperationsTest {
+    private final ArrayOperationsImpl arrayOperations = new ArrayOperationsImpl();
+
     @Test
     public void sumUsingStreams() throws Exception {
-        ArrayOperationsImpl arrayOperations = new ArrayOperationsImpl();
         assertThat(arrayOperations.sumUsingStreams(null), is(0));
         assertThat(arrayOperations.sumUsingStreams(new int[]{}), is(0));
         assertThat(arrayOperations.sumUsingStreams(new int[]{1}), is(1));
@@ -18,7 +19,6 @@ public class ArrayOperationsTest {
 
     @Test
     public void sumUsingForLoop() {
-        ArrayOperationsImpl arrayOperations = new ArrayOperationsImpl();
         assertThat(arrayOperations.sumUsingForLoop(null), is(0));
         assertThat(arrayOperations.sumUsingForLoop(new int[]{}), is(0));
         assertThat(arrayOperations.sumUsingForLoop(new int[]{1}), is(1));
@@ -27,7 +27,6 @@ public class ArrayOperationsTest {
 
     @Test
     public void sumUsingWhileLoop() {
-        ArrayOperationsImpl arrayOperations = new ArrayOperationsImpl();
         assertThat(arrayOperations.sumUsingWhileLoop(null), is(0));
         assertThat(arrayOperations.sumUsingWhileLoop(new int[]{}), is(0));
         assertThat(arrayOperations.sumUsingWhileLoop(new int[]{1}), is(1));
@@ -36,7 +35,6 @@ public class ArrayOperationsTest {
 
     @Test
     public void sumUsingRecursion() {
-        ArrayOperationsImpl arrayOperations = new ArrayOperationsImpl();
         assertThat(arrayOperations.sumUsingRecursion(null), is(0));
         assertThat(arrayOperations.sumUsingRecursion(new int[]{}), is(0));
         assertThat(arrayOperations.sumUsingRecursion(new int[]{1}), is(1));
@@ -45,7 +43,6 @@ public class ArrayOperationsTest {
 
     @Test
     public void numberOccurringOddNumberOfTimes() {
-        ArrayOperationsImpl arrayOperations = new ArrayOperationsImpl();
         assertThat(arrayOperations.numberOccurringOddNumberOfTimes(null), is(-1));
         assertThat(arrayOperations.numberOccurringOddNumberOfTimes(new int[0]), is(-1));
         assertThat(arrayOperations.numberOccurringOddNumberOfTimes(new int[]{1, 1}), is(-1));
@@ -57,7 +54,6 @@ public class ArrayOperationsTest {
 
     @Test
     public void numbersOccurringOddNumberOfTimes() throws Exception {
-        ArrayOperationsImpl arrayOperations = new ArrayOperationsImpl();
         assertThat(arrayOperations.numbersOccurringOddNumberOfTimes(null), is(new int[0]));
         assertThat(arrayOperations.numbersOccurringOddNumberOfTimes(new int[0]), is(new int[0]));
         assertThat(arrayOperations.numbersOccurringOddNumberOfTimes(new int[]{1, 1}), is(new int[0]));
@@ -65,6 +61,19 @@ public class ArrayOperationsTest {
         assertThat(arrayOperations.numbersOccurringOddNumberOfTimes(new int[]{1, 1, 2, 3, 3}), is(new int[]{2}));
         assertThat(arrayOperations.numbersOccurringOddNumberOfTimes(new int[]{1, 1, 2, 2, 3, 4, 5}), is(new int[]{3, 4, 5}));
         assertThat(arrayOperations.numbersOccurringOddNumberOfTimes(new int[]{1, 1, 2, 2, 3, 3, 5, 5, 5}), is(new int[]{5}));
+    }
+
+    @Test
+    public void findSumTuples() {
+        assertThat(arrayOperations.findSumTuples(null, 10), is(new int[0][]));
+        assertThat(arrayOperations.findSumTuples(new int[0], 10), is(new int[0][]));
+        assertThat(arrayOperations.findSumTuples(new int[0], -1), is(new int[0][]));
+        assertThat(arrayOperations.findSumTuples(new int[]{1, 2, 3}, 10), is(new int[0][]));
+        assertThat(arrayOperations.findSumTuples(new int[]{1, 2, 3}, -1), is(new int[0][]));
+        assertThat(arrayOperations.findSumTuples(new int[]{3, 5}, 8), is(new int[][]{{3, 5}}));
+        assertThat(arrayOperations.findSumTuples(new int[]{1, 2, 4, -1}, 3), is(new int[][]{{1, 2}, {4, -1}}));
+        assertThat(arrayOperations.findSumTuples(new int[]{1, 2, 3, 4, 5, 6, 7, 10, -2, -7}, 3),
+                is(new int[][]{{1, 2}, {5, -2}, {10, -7}}));
 
     }
 }
